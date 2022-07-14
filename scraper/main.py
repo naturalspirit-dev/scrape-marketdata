76ec74cf20933be883e992e1af7c8d83faa96a3e
romanmorris <natural.spirit0102@gmail.com>
2022-07-13 21:32:41 -1000

    tweak for express server

--- scraper/main.py
+++ scraper/main.py
@@ -19,32 +19,53 @@ def waitForOpen():
     else:
         time.sleep(timeToOpen())
 
+def run():
+    auth()
+    dataHarvester = HarvestData()
+    currEst = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    # currEstHour = int(str(currEst.time()).split(':')[0])
+
+    if (currEst.weekday() == 5 or currEst.weekday() == 6):
+        print("It's the weekend - exiting...")
+        return
+
+    waitForOpen()
+
+    if (not rs.get_market_today_hours('XNYS')['is_open']):
+        print("Market is currently closed - exiting...")
+        return
+
+    else:
+        print("--- Running data harvester ", datetime.datetime.now(), "---")
+        while (currEst.time() < datetime.time(16, 20, 0, 0, pytz.timezone('US/Eastern'))): ##until market closes (EST), run bot every 5 minute
+            dataHarvester.harvestDailyDataToDB("spy")
+            dataHarvester.harvestDailyDataToDB("aapl")
+            dataHarvester.harvestDailyDataToDB("meta")
+            dataHarvester.harvestDailyDataToDB("amc")
+            dataHarvester.harvestDailyDataToDB("nvda")
+            dataHarvester.harvestDailyDataToDB("tsla")
+            dataHarvester.harvestDailyDataToDB("amzn")
+            dataHarvester.harvestDailyDataToDB("goog")
+            dataHarvester.harvestDailyDataToDB("mrna")
+            dataHarvester.harvestDailyDataToDB("twtr")
+            dataHarvester.harvestDailyDataToDB("msft")
+            time.sleep(300)
+        print("--- Finished running data harvester ---")
+        return
+
 auth()
 dataHarvester = HarvestData()
-currEst = datetime.datetime.now(pytz.timezone('US/Eastern'))
-currEstHour = int(str(currEst.time()).split(':')[0])
-
-if (currEst.weekday() == 5 or currEst.weekday() == 6):
-    print("It's the weekend - exiting...")
-    exit()
-
-waitForOpen()
-if (not rs.get_market_today_hours('XNYS')['is_open']):
-    print("Market is currently closed - exiting...")
-    exit()
-else:
-    print("--- Running data harvester ", datetime.datetime.now(), "---")
-    while (currEst.time() < datetime.time(16, 20)): ##until market closes (EST), run bot every 5 minute
-        dataHarvester.harvestDailyDataToDB("spy")
-        dataHarvester.harvestDailyDataToDB("aapl")
-        dataHarvester.harvestDailyDataToDB("meta")
-        dataHarvester.harvestDailyDataToDB("amc")
-        dataHarvester.harvestDailyDataToDB("nvda")
-        dataHarvester.harvestDailyDataToDB("tsla")
-        dataHarvester.harvestDailyDataToDB("amzn")
-        dataHarvester.harvestDailyDataToDB("goog")
-        dataHarvester.harvestDailyDataToDB("mrna")
-        dataHarvester.harvestDailyDataToDB("twtr")
-        dataHarvester.harvestDailyDataToDB("msft")
-        time.sleep(300)
-    print("--- Finished running data harvester ---")
+dataHarvester.writeCurrentDataToDB("spy")
+dataHarvester.writeCurrentDataToDB("aapl")
+dataHarvester.writeCurrentDataToDB("meta")
+dataHarvester.writeCurrentDataToDB("amc")
+dataHarvester.writeCurrentDataToDB("nvda")
+dataHarvester.writeCurrentDataToDB("tsla")
+dataHarvester.writeCurrentDataToDB("amzn")
+dataHarvester.writeCurrentDataToDB("goog")
+dataHarvester.writeCurrentDataToDB("mrna")
+dataHarvester.writeCurrentDataToDB("twtr")
+dataHarvester.writeCurrentDataToDB("msft")
+# while (True): 
+#     run() 
+#     time.sleep(82800) #run once a day
